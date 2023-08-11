3399d045efce63a63fe5e8b84c6f0f3a8972a7ac
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-08-11 20:27:10 +0700

    Rewrite dwmblocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,14 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    {"", "~/.dwmblocks/scripts/kernel.sh", 600, 0},
-
-    {"", "~/.dwmblocks/scripts/uptime.sh", 1, 0},
-
-    {"", "~/.dwmblocks/scripts/cpu.sh", 1, 0},
-
-    {"", "~/.dwmblocks/scripts/ram.sh", 1, 0},
-
-    {"", "~/.dwmblocks/scripts/date.sh", 1, 0},
+    /*Icon*/	/*Command*/		    /*Update Interval*/	/*Update Signal*/
+    {"", "$HOME/.dwmblocks/scripts/kernel.sh",	600,		2},
+    {"", "$HOME/.dwmblocks/scripts/uptime.sh",	30,		2},
+    {"", "$HOME/.dwmblocks/scripts/cpu.sh",	1,		1},
+    {"", "$HOME/.dwmblocks/scripts/ram.sh",	1,		1},
+    {"", "$HOME/.dwmblocks/scripts/date.sh",	1,		0},
 };
 
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
